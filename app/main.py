ad4c7eb1cf8f1f119c0da6bea8a74f104d39143b
VibhavSingh1 <vibhav.singh0v@gmail.com>
2025-04-08 07:18:15 +0530

    Made minor modification to log app startup and shutdown messages

--- app/main.py
+++ app/main.py
@@ -7,18 +7,23 @@ loaded from the settings and registers API routers.
 """
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.routes import summarizer
 from app.core.settings import settings
 from app.core.loggers import logger
 
-logger.info(f"Starting {settings.app_name} v{settings.version} in {settings.environment} mode.")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info(f"Starting app {settings.app_name} v{settings.version} in {settings.environment} mode.")
+    yield
+    logger.info(f"🛑 Shutting app {settings.app_name} v{settings.version} in {settings.environment} mode.")  
 
 app = FastAPI(
     title=settings.app_name,
     description=settings.description,
-    version=settings.version
+    version=settings.version,
+    lifespan=lifespan
 )
 
 # Register routers
 app.include_router(summarizer.router, prefix="/summarizer", tags=["Summarizer"])
-logger.info("Router registered: /summarizer")
